cf31588d2209fa3018e8e3b84e4215b20909150e
WarpZephyr <107563368+WarpZephyr@users.noreply.github.com>
2025-11-30 22:17:38 -0500

    Fixed deflate compress, rewrote deflate decompress and compress

--- MordorFormats/Compression/Deflate.cs
+++ MordorFormats/Compression/Deflate.cs
@@ -15,31 +15,20 @@ namespace MordorFormats.Compression
         /// <param name="source">The source to decompress from.</param>
         /// <param name="destination">The destination to decompress to.</param>
         /// <returns>The amount decompressed.</returns>
-        /// <exception cref="Exception">Couldn't read all data to decompress.</exception>
         public static unsafe int Decompress(ReadOnlySpan<byte> source, Span<byte> destination)
         {
             fixed (byte* pSource = &source[0])
             {
                 using var inStream = new UnmanagedMemoryStream(pSource, source.Length);
+                using var outStream = new MemoryStream(destination.Length);
                 using var deflateStream = new DeflateStream(inStream, CompressionMode.Decompress);
+                deflateStream.CopyTo(outStream);
+                deflateStream.Flush();
 
-                int remaining = destination.Length;
-                int totalRead = 0;
-                int read;
-                do
-                {
-                    read = deflateStream.Read(destination.Slice(totalRead, remaining));
-                    totalRead += read;
-                    remaining -= read;
-
-                    if (read == 0 && remaining > 0)
-                    {
-                        throw new Exception("Hit the end of deflate stream before getting all expected data.");
-                    }
-                }
-                while (remaining > 0);
-
-                return totalRead;
+                int written = (int)outStream.Length;
+                outStream.Position = 0;
+                outStream.ReadExactly(destination[..written]);
+                return written;
             }
         }
 
@@ -49,35 +38,20 @@ namespace MordorFormats.Compression
         /// <param name="source">The source to compress from.</param>
         /// <param name="destination">The destination to compress to.</param>
         /// <returns>The amount compressed.</returns>
-        /// <exception cref="Exception">Couldn't compress all data.</exception>
         public static unsafe int Compress(ReadOnlySpan<byte> source, Span<byte> destination)
         {
             fixed (byte* pSource = &source[0])
             {
                 using var inStream = new UnmanagedMemoryStream(pSource, source.Length);
-                using var deflateStream = new DeflateStream(inStream, CompressionMode.Compress);
+                using var outStream = new MemoryStream(destination.Length);
+                using var deflateStream = new DeflateStream(outStream, CompressionMode.Compress);
+                inStream.CopyTo(deflateStream);
+                deflateStream.Flush();
 
-                int remaining = destination.Length;
-                int totalWritten = 0;
-                int written;
-
-                do
-                {
-                    long start = deflateStream.Position;
-                    deflateStream.Write(destination);
-                    long end = deflateStream.Position;
-                    written = (int)(end - start);
-                    totalWritten += written;
-                    remaining -= written;
-
-                    if (written == 0 && remaining > 0)
-                    {
-                        throw new Exception("Could not write all data to deflate stream.");
-                    }
-                }
-                while (remaining > 0);
-
-                return totalWritten;
+                int written = (int)outStream.Length;
+                outStream.Position = 0;
+                outStream.ReadExactly(destination[..written]);
+                return written;
             }
         }
     }
